d689ac1cbeae8c530a054d730b8706ca3625bd29
yangqiangcode <1304397531@qq.com>
2022-11-02 12:07:46 +0800

    feat: cluster

--- config/config.default.ts
+++ config/config.default.ts
@@ -35,6 +35,14 @@ export default (appInfo: EggAppInfo) => {
       secret: 'xuyiyang-website-jwt-key',
     },
 
+    cluster: {
+      listen: {
+        port: 7001,
+        hostname: '0.0.0.0', // 不建议设置 hostname 为 '0.0.0.0'，它将允许来自外部网络和来源的连接，请在知晓风险的情况下使用
+        // path: '/var/run/egg.sock',
+      },
+    },
+
     // Enable CORS
     cors: {
       origin: '*',

9ec2a2f0e2db62210627e0278848de393adf7847
yangqiangcode <1304397531@qq.com>
2022-12-08 17:23:32 +0800

    delete: enums table

--- app/typings/enum.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from 'mongoose';
-
-export interface EnumType extends Document {
-  options: {
-    label: string;
-    value: string;
-  };
-  name: string;
-}
--- typings/app/model/index.d.ts
+++ typings/app/model/index.d.ts
@@ -3,14 +3,12 @@
 
 import 'egg';
 import ExportBannerModel from '../../../app/model/BannerModel';
-import ExportEnumModel from '../../../app/model/enumModel';
 import ExportMessageBoardModel from '../../../app/model/MessageBoardModel';
 import ExportUserModel from '../../../app/model/UserModel';
 
 declare module 'egg' {
   interface IModel {
     BannerModel: ReturnType<typeof ExportBannerModel>;
-    EnumModel: ReturnType<typeof ExportEnumModel>;
     MessageBoardModel: ReturnType<typeof ExportMessageBoardModel>;
     UserModel: ReturnType<typeof ExportUserModel>;
   }

d39c338ef1096f0a99d3ec5491edc666f8a52482
gu ming <gumingcloud@163.com>
2023-07-31 18:02:51 +0800

    fix: api auth

--- app/router/UserRouter.ts
+++ app/router/UserRouter.ts
@@ -5,9 +5,9 @@ export default (app: Application) => {
   const { userController } = controller;
 
   // 以 非 /public/ 访问的接口都是需要 校验 token 的
-  router.post('/public/loginUser', userController.loginUser);
-  router.post('/public/loginAdmin', userController.loginAdmin);
-  router.post('/public/loginOut', userController.loginOut);
+  router.post('/public/auth/loginUser', userController.loginUser);
+  router.post('/public/auth/loginAdmin', userController.loginAdmin);
+  router.post('/public/auth/loginOut', userController.loginOut);
 
   router.post(
     '/public/user/sendUpdatePasswordEmailCaptcha',
